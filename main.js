4a908e1016d6c749a9ee3183242a29f0d958cf7e
Yesid <yesidrodriguez305@gmail.com>
2022-08-08 15:43:34 -0500

    Clase 22. Fusión de detalles del producto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const burguerMenu = document.querySelector('.menu');
 const navEmail = document.querySelector('.navbar-email');
 const cartMenuIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
@@ -11,20 +11,20 @@ burguerMenu.addEventListener('click', toggleMobileMenu);
 cartMenuIcon.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
   if (!isAsideClosed) {
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
 
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
   if (!isAsideClosed) {
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
 
   mobileMenu.classList.toggle('inactive');
@@ -42,7 +42,7 @@ function toggleCartAside() {
     desktopMenu.classList.add('inactive');
   }
 
-  aside.classList.toggle('inactive');
+  shoppingCartContainer.classList.toggle('inactive');
 }
 
 // TODO EN UNA FUNCION
